f4a730d1081c7aa5a42feb40435c04c6442d007c
cre8tiv <bmartin@cre8tivsystems.com>
2018-03-19 10:25:41 -0400

    adding database settings

--- database/restore.sh
+++ database/restore.sh
@@ -2,5 +2,5 @@
 cp /tmp/backup/DemoDb.bak /var/opt/mssql/data/DemoDb.bak
 
 /opt/mssql-tools/bin/sqlcmd \
--S 127.0.0.1 -U SA -P HereIsThePa$$word! \
+-S 127.0.0.1 -U SA -P HereIsThePassword! \
 -i /tmp/backup/restore.sql
